57c4ec30c856ad0fefbbe5478354d84b6575059c
gfloresuth <dexterminio@gmail.com>
2016-01-11 15:24:55 -0700

    Se agrego un elemento visual nuevo y se muestra el mismo ejemplo.

--- CSDemoGDI/DemosVisualesGDI.cs
+++ CSDemoGDI/DemosVisualesGDI.cs
@@ -8,20 +8,22 @@ namespace CSDemoGDI
 {
     class DemosVisualesGDI
     {
+        TextoVisual tVisual;
         Bitmap imagen;
         PictureBox pic;
         Font fuente;
         Graphics g;
         int numero;
-        Point posTexto;
+        //Point posTexto;
         public DemosVisualesGDI(PictureBox pantalla)
         {
             fuente = new Font("Courier New", 10);
-            posTexto = new Point(10, 10);
+            //posTexto = new Point(10, 10);
+            numero = 0;
+            tVisual = new TextoVisual(10, 10, "El numero es: " + numero);
             pic = pantalla;
             imagen = new Bitmap(pic.Width, pic.Height);
             g = Graphics.FromImage(imagen);
-            numero = 0;
         }
         public void mostrar()
         {
@@ -30,7 +32,9 @@ namespace CSDemoGDI
         public void contar()
         {
             g.Clear(Color.Black);
-            g.DrawString("El numero es: " + numero, fuente, Brushes.White, posTexto);
+            //g.DrawString("El numero es: " + numero, fuente, Brushes.White, posTexto);
+            tVisual.setTexto("El numero es: " + numero);
+            tVisual.pintar(g, fuente, Brushes.White);
             numero++;
             if (numero > 1000)
             {
---
+++ CSDemoGDI/TextoVisual.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace CSDemoGDI
+{
+    class TextoVisual
+    {
+        private Point _pos;
+        private string _texto;
+        public TextoVisual(int x, int y, string unTexto)
+        {
+            _pos = new Point(x, y);
+            _texto = unTexto;
+        }
+        public void setTexto(string valor)
+        {
+            _texto = valor;
+        }
+        public string getTexto()
+        {
+            return _texto;
+        }
+        public void pintar(Graphics g,Font unaFuente,Brush unColor)
+        {
+            g.DrawString(_texto, unaFuente, unColor, _pos);
+        }
+    }
+}
